ee8f06807bac44845ba08e3bd03aa76cd92f888f
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-02-15 20:18:26 -0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-var express = require('express');
+var Express = require('express');
+var app = Express();
+var	Mongoose = require('mongoose');
+var MongoUri = 'mongodb://localhost:27017/song-tag';
+var BodyParser = require('body-parser');
 
-var app = express();
+var port = 9099
+
+app.use(BodyParser.json());
+
+// Controllers
+var ArtistCtrl = require('./lib/controllers/artistCtrl');
+
+Mongoose.connect(MongoUri);
+Mongoose.connection.once('open', function() {
+	console.log('Connected to db ' + MongoUri);
+});
 
 //routes go here
+app.post('/artist', ArtistCtrl.post);
 
-app.listen(8000);
+
+app.listen(port);
+
